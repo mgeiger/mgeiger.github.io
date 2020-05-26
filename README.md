e7a005af888f124d45c2a2811f5401d55e03a195
Matthew J. Geiger <matthew.j.geiger@gmail.com>
2020-05-26 16:58:17 -0400

    Add README.md file

---
+++ README.md
@@ -0,0 +1,13 @@
+mgeiger.github.io
+=================
+
+Publishing
+----------
+
+* Make your changes on the `content` branch
+* Run `make publish`
+* Run `ghp-import -m "Change Comments" --no-jekyll -b master output
+* Push master commits `git push origin master`
+* Commit local changes `git commit -a -m "Change Comments"`
+* Push Content changes `git push origin content`
+
--- pelicanconf.py
+++ pelicanconf.py
@@ -2,16 +2,19 @@
 # -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
+# General Settings
 AUTHOR = 'Matthew Geiger'
 SITENAME = 'Geiger Labs Blog'
+#SITEURL = 'https://mgeiger.github.io'
 SITEURL = ''
-
-PATH = 'content'
-
+ABOUT_ME = "Software Automation Engineer who loves Python"
 TIMEZONE = 'America/New_York'
-
 DEFAULT_LANG = 'en'
 
+# Paths
+PATH = 'content'
+
+# Feeds
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
 CATEGORY_FEED_ATOM = None
@@ -19,18 +22,20 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
+# Social
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
-
+# LINKS = (('Pelican', 'http://getpelican.com/'),
+#          ('Python.org', 'http://python.org/'),
+#          ('Jinja2', 'http://jinja.pocoo.org/'),
+#          ('You can modify those links in your config file', '#'),)
+LINKS = None
 # Social widget
 SOCIAL = (('LinkedIn', 'https://www.linkedin.com/in/matthewjgeiger'),
-        ('GitHub', 'https://github.com/mgeiger'),)
-
-DEFAULT_PAGINATION = 10
-SUMMARY_MAX_LENGTH = 50
+        ('GitHub', 'https://github.com/mgeiger'),
+        ('Twitter', 'https://twitter.com/mgeiger1980'))
+GITHUB_USER = 'mgeiger'
+GITHUB_SKIP_FORK = True
+GITHUB_REPO_COUNT = 2
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -39,11 +44,18 @@ STATIC_PATHS = ['extra/robots.txt', 'extra/favicon.ico']
 EXTRA_PATH_METADATA = {
         'extra/robots.txt': {'path': 'robots.txt'},
         'extra/favicon.ico': {'path': 'favicon.ico'},
-        }
-TYPOGRIFY = True
+}
 PLUGIN_PATHS = ['../pelican-plugins/', ]
 PLUGINS = ['i18n_subsites', ]
 JINJA_ENVIRONMENT = {
         'extensions': ['jinja2.ext.i18n'],
         }
+# Rendering
+TYPOGRIFY = True
 THEME = '../pelican-themes/pelican-bootstrap3'
+BOOTSTRAP_THEME = 'flatly'
+DEFAULT_PAGINATION = 5
+# SUMMARY_MAX_LENGTH = 50
+DISPLAY_TAGS_ON_SIDEBAR = True
+DISPLAY_TAGS_INLINE = True
+DISPLAY_CATEGORIES_ON_MENU = False
